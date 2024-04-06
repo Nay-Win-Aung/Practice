08f81fc96c69b2de0bea17663fafb2d703ad85c5
Nay Win Aung <naywinaung2102022@gmail.com>
2024-04-06 21:36:21 +0630

    one

---
+++ script.js
@@ -0,0 +1,16 @@
+//import express
+const express = require("express");
+//create app
+const app = express();
+//defining routes with get request handler
+app.get("/api/nay",function(req,res){
+    const data =[
+        {name:"nay"},
+        {age:41}
+    ];
+    return res.status(200).json(data);
+});
+//app listen
+app.listen(8000,function(){
+    console.log("server is running ");
+});
